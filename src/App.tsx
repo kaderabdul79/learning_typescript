422906632d4b75b7e7b0e6307bcdb9f16e475e22
kaderabdul <kaderabdul7980@gmail.com>
2022-05-07 10:16:55 +0600

    need to fix error inside the rendernote function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import {Note} from './models/note.model';
+import NotesList from './components/NotesList';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function App() {
   const [notes,setNotes] = useState<Note[]>([{
@@ -15,6 +17,12 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Container>
+        <Row>
+          <Col><NotesList notes={notes} /></Col>
+        </Row>
+      </Container>
+      
     </div>
   );
 }
---
+++ src/components/Notes.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Note } from '../models/note.model';
+
+interface INotesProps {
+    note: Note[]
+}
+
+const Notes: React.FunctionComponent<INotesProps> = ({note}) => {
+  return (
+    <div>Note Card</div>
+  );
+};
+
+export default Notes;
